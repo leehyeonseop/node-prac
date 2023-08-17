f25964a37051bb80c9ef808eff17ab675f5c8d92
leehyeonseop <gustjq4146@naver.com>
2023-08-17 22:39:14 +0900

    Section3 fin

--- section-3/session.js
+++ section-3/session.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs').promises;
 const url = require('url');
 const qs = require('querystring');
+const path = require('path');
 
 const parseCookies = (cookie = '') =>
     cookie
@@ -12,36 +13,41 @@ const parseCookies = (cookie = '') =>
             return acc;
         }, {});
 
-http.createServer(async (req, res) => {
-    const cookies = parseCookies(req.headers.cookie) // { mycookie : 'test' }
+const session = {};
 
+http.createServer(async (req, res) => {
+    const cookies = parseCookies(req.headers.cookie);
     if (req.url.startsWith('/login')) {
         const { query } = url.parse(req.url);
         const { name } = qs.parse(query);
         const expires = new Date();
         expires.setMinutes(expires.getMinutes() + 5);
-        
+        const uniqueInt = Date.now();
+        session[uniqueInt] = {
+            name,
+            expires,
+        };
+
         res.writeHead(302, {
             Location : '/',
-            'Set-Cookie' : `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HTTPOnly; Path=/`,
+            'Set-Cookie' : `session=${uniqueInt}; Expires=${expires.toGMTString()}; HTTPOnly; Path=/`
         });
-
         res.end();
-    } else if (cookies.name) {
+        // 세션 쿠키가 존재하고, 만료 기간이 지나지 않았다면,
+    } else if (cookies.session && session[cookies.session].expires > Date.now()) {
         res.writeHead(200, { 'Content-Type' : 'text/plain; charset=utf-8' });
-        res.end(`${cookies.name}님 안녕하세요.`);
+        res.end(`${session[cookies.session].name}님 안녕하세요.`)
     } else {
         try {
-            const data = await fs.readFile('./cookie2.html');
+            const data = await fs.readFile(path.join(__dirname, 'cookie2.html'));
             res.writeHead(200, { 'Content-Type' : 'text/html; charset=utf-8' });
-            res.end(data);
-        } catch (err) {
+            res.end(data)
+        } catch(err) {
             res.writeHead(500, { 'Content-Type' : 'text/plain; charset=utf-8' });
-            res.end(err.message)
+            res.end(err.message);
         }
     }
-
 })
-    .listen(8084, () => {
-        console.log('8084번 포트에서 서버 대기중')
+    .listen(8085, () => {
+        console.log('8085번 포트에서 서버 대기중')
     })
